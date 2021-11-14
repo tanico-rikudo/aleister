ee3c4d6632a25c3ff483ac21ed02d331550e2bb3
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-11-14 11:29:03 +0900

    move config handler

--- src/base_process.py
+++ src/base_process.py
@@ -1,26 +1,25 @@
+from util.config import ConfigManager
+
+cm = ConfigManager(os.environ['ALEISTER_INI'])
+
 class BaseProcess():
     def __init__(self, _id, logger):
         self._logger  = logger
         self.id = _id
 
     def load_general_config(source="ini", path=None,dict_obj=None, mode=None):
-        config_ini = configparser.ConfigParser()
         if source == "ini":
-            path = '../ini/config.ini' if path is None else path
-            self.general_config = config_ini.read(path, encoding='utf-8')[mode]
+            self.general_config = cm.load_ini_config(path=path,config_name="general", mode=mode)
         elif source == "dict":
-            self.general_config = parser.read_dict(dict_obj)[mode]
+            self.general_config = cm.load_dict_config(dict_obj)[mode]
         self.save_dir = os.path.join(self.general_config.get("MODEL_SAVE_PATH"), self.id)
         self._logger.info('[DONE]Load General. Source={0}'.format(source))
         
     def load_model_config(source="ini", path=None,dict_obj=None, odel_name=None):
-        config_ini = configparser.ConfigParser()
         if source == "ini":
-            model_config_ini = configparser.ConfigParser()
-            path = '../ini/model_config.ini' if path is None else path
-            self.model_config = model_config_ini.read(path, encoding='utf-8')[model_name]
-        elif source == "dict":          
-            self.general_config = parser.read_dict(dict_obj)[mode]
+            self.general_config = cm.load_ini_config(path=path,config_name="model", mode=mode)
+        elif source == "dict":
+            self.general_config = cm.load_dict_config(dict_obj)[mode]
         self._logger.info('[DONE]Load Model Config. Source={0}'.format(source))
         
 
