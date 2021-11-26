1fc63bf537bc262eaf1a06a8335475159fc42728
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-11-26 20:07:20 +0900

    update model  mosules

--- src/model_modules.py
+++ src/model_modules.py
@@ -17,6 +17,21 @@ class parameterParser:
         hparams["optimizer"] = model_config.get("OPTIMIZER")
         hparams["loss_fn"] = model_config.get("LOSSFN")
         return hparams
+    
+    @staticmethod
+    def lstm(model_config):
+        hparams = {}
+        hparams["n_epoch"] = model_config.getint("N_EPOCH")
+        hparams["batch_size"] = model_config.getint("BATCHSIZE")
+        hparams["hidden_dim"] = model_config.getint("HIDDEN_DIM")
+        hparams["layer_dim"] = model_config.getint("LAYER_DIM")
+        hparams["out_dim"] = model_config.getint("OUT_DIM")
+        hparams["l2_drop_rate"] = model_config.getfloat("L2_DROP_RATE")
+        hparams["weight_decay"] = model_config.getfloat("WEIGHT_DECAY")
+        hparams["lr"] = model_config.getfloat("LR")
+        hparams["optimizer"] = model_config.get("OPTIMIZER")
+        hparams["loss_fn"] = model_config.get("LOSSFN")
+        return hparams
         
     
 class SimpleDnn(nn.Module):
@@ -54,3 +69,38 @@ class SimpleDnn(nn.Module):
         
         return x
     
+
+class SLSTM(nn.Module):
+
+    def __init__(self, dimension=128):
+        super(LSTM, self).__init__()
+
+        self.embedding = nn.Embedding(len(text_field.vocab), 300)
+        self.dimension = dimension
+        self.lstm = nn.LSTM(input_size=300,
+                            hidden_size=dimension,
+                            num_layers=1,
+                            batch_first=True,
+                            bidirectional=True)
+        self.drop = nn.Dropout(p=0.5)
+
+        self.fc = nn.Linear(2*dimension, 1)
+
+    def forward(self, text, text_len):
+
+        text_emb = self.embedding(text)
+
+        packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
+        packed_output, _ = self.lstm(packed_input)
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
+
+        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
+        out_reverse = output[:, 0, self.dimension:]
+        out_reduced = torch.cat((out_forward, out_reverse), 1)
+        text_fea = self.drop(out_reduced)
+
+        text_fea = self.fc(text_fea)
+        text_fea = torch.squeeze(text_fea, 1)
+        text_out = torch.sigmoid(text_fea)
+
+        return text_out
