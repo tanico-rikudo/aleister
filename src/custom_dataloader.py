65c49337a2a63de6fe5932a36e601a027494b7a5
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-11-26 20:01:59 +0900

    Fix sequence multi loader

--- src/custom_dataloader.py
+++ src/custom_dataloader.py
@@ -1,27 +1,39 @@
 import torch
 from torch.utils.data import Dataset
 
+import numpy  as np
+
 class SequenceDataset(Dataset):
     def __init__(self, *tensors, **params):
         self.X_tensors = tensors[:-1]
         self.y_tensor = tensors[-1]
-        self.sequence_length = params["sequence_length"]
+        self.window_size = params["window_size"]
         self.batch_size = params["batch_size"]
-        self.Xs = X_tensors.float()
-        self.y = y_tensor.float()
+        self.Xs = [ X_tensor.numpy() for X_tensor in self.X_tensors]
+        self.y = self.y_tensor.numpy()
 
     def __len__(self):
-        return self.X.shape[0]
+        return self.Xs[0].shape[0]
 
     def __getitem__(self, idx): 
-        if idx >= self.sequence_length - 1:
-            idx_start = idx - self.sequence_length + 1
+        if idx >= (self.window_size - 1):
+            idx_start = idx - self.window_size + 1
             Xs = []
-            for X in self.X:
+            for X in self.Xs:
                 Xs.append(X[idx_start:idx+1:])
         else:
-            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
-            x = self.X[0:(i + 1), :]
-            x = torch.cat((padding, x), 0)
-
-        return Xs, self.y[i]
+            n_padding = self.window_size - idx - 1
+            Xs = []
+            for X in self.Xs:
+                padding = np.repeat(X[0][None,:], n_padding, axis=0)
+                X_origin = X[:idx+1]
+                X_padded = np.vstack([X_origin, padding])
+                Xs.append(X_padded)
+                
+        return Xs, self.y[idx]
+    
+    # def __getshape__(self):
+    #     return (self.__len__(), *self.__getitem__(0)[0].shape)
+    
+    # def __getsize__(self):
+    #     return (self.__len__())
