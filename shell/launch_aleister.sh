a3b680a86db797902c81e9dea99cdb0cb3eea7d7
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2021-12-25 15:01:20 +0900

    add shell

--- shell/launch_aleister.sh
+++ shell/launch_aleister.sh
@@ -1,47 +1,47 @@
-
-#!/bin/bash
-
-set -euxo pipefail
+#!/bin/bash 
+set -euxo pipefail 
 . cat /shell_config.conf
 
 function usage() {
 cat <<_EOT_
 Usage:
-  $0 [-a] [-b] [-f filename] arg1 ...
+  $0 [-u user] [-s symbol] [-i filename] [-m model name] arg1 ...
 
 Description:
   Launch aleister operation
 
 Options:
+  -u   user
   -s   symbol
-  -p    prepro
-  -l    learn/train
   -i   model id
-  -n  model name
-  -
+  -m   model name
 _EOT_
 exit 1
 }
 
+SOURCE=shell
+MODE=prepro
+CONFIGSOURCE=ini
+CONFIGMODE=default
+
 
 if [ "$OPTIND" = 1 ]; then
-  while getopts a:b:f:h: OPT
+  while getopts u:s:i:m:h OPT
   do
     case $OPT in
-      s)
-        symbol=$OPTARG
-        echo "FLAG_A is $FLAG_A"            # for debug
+      u)
+        USER=$OPTARG
         ;;
-      e)
-        FLAG_B="on"
-        echo "FLAG_B is $FLAG_B"            # for debug
+      s)
+        SYMBOL=$OPTARG
         ;;
       i)
-        ARG_F=$OPTARG
-        echo "ARG_F is $ARG_F"              # for debug
+        ID=$OPTARG
         ;;
-      n)
-        echo "h option. display help"       # for debug
+      m)
+        MODEL=$OPTARG
+        ;;
+      h)
         usage
         ;;
       \?)
@@ -53,3 +53,34 @@ else
   echo "No installed getopts-command." 1>&2
   exit 1
 fi
+
+shift $((OPTIND - 1))
+
+# prepro 
+if [ $(( $# & 1 )) -eq 1 ]; then 
+  echo odd period options
+  exit 0
+fi
+if [ $# -eq 2 ]; then 
+  train_start_date=$1
+  train_end_date=$2
+  train_period_opt="--train_start_date ${train_start_date} --train_end_date ${train_end_date}"
+fi
+if [ $# -ge 4 ]; then 
+  valid_start_date=$3
+  valid_end_date=$4
+  valid_period_opt="--valid_start_date ${valid_start_date} --valid_end_date ${valid_end_date}"
+fi
+if [ $# -ge 6 ]; then 
+  test_start_date=$5
+  test_end_date=$6
+  test_period_opt="--test_start_date ${test_start_date} --test_end_date ${test_end_date}"
+fi
+source deactivate
+source activate py37
+python_interpritor=python
+execute_path=`dirname $(pwd)`
+execute_path="${execute_path}/src"
+cd ${execute_path}
+commnad="${python_interpritor} master.py -u ${USER} -s ${SOURCE} -sym ${SYMBOL} -mode ${MODE} -cs ${CONFIGSOURCE} -cm ${CONFIGMODE} -id ${ID} -mn ${MODEL} "
+eval "${commnad} ${train_period_opt} ${valid_period_opt} ${test_period_opt} " 
